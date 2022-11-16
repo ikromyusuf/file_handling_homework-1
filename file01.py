3411e30918bdecda42baebf3c1eface85f1acd6b
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-11-16 14:57:39 +0500

    1 done

--- file01.py
+++ file01.py
@@ -6,5 +6,10 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    l = []
+    for i in data.split(","):
+        l.append(int(i))
+    return l
+f = open("txt_file/data01.txt")
+print(main(f.read()))
 # Read data from file

2bf4c9b10b4ab261035054e824685d36906f06dd
ikromyusuf <ikromyusupov09@gmail.com>
2023-02-09 17:20:15 +0500

    done

--- file06.py
+++ file06.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    data = data.split("\n")
+    l = []
+    for i in data:
+        l.append(len(i))
+    return l
+data = open("txt_file/data06.txt").read()
+print(main(data))
 # Read data from file
